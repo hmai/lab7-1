0e4368ba2ad8b2122a51ac6f7c924801850ac957
hoa mai <hmai_23@yahoo.com>
2016-02-18 21:21:53 +0000

    analytics lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -5,9 +5,19 @@ $(document).ready(function() {
 	initializePage();
 });
 
+function likeClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event    
+    ga("send", "event", "likeBtn", "click");
+}
+
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	// your code here
+
+	$(".likeBtn").click(likeClick);
 }
